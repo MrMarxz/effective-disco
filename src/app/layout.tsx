482d7eabef028e6abcd155a9cb4a5f28a22e45b6
MrMarxz <markus.marais@outlook.com>
2024-10-12 22:11:05 +0200

    Added umami

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 import { CredentialsProvider } from "~/components/CredentialsProvider";
 import { Toaster } from 'react-hot-toast';
+import { env } from "~/env";
 
 export const metadata: Metadata = {
   title: "JaKaMa",
@@ -16,6 +17,9 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
+      <head>
+        {env.NODE_ENV === "production" && <script defer src="https://cloud.umami.is/script.js" data-website-id="bdbfd1a9-feff-4bd2-9b59-0f0ba723e2ae"></script>}
+      </head>
       <body>
         <Toaster />
         <CredentialsProvider>{children}</CredentialsProvider>
